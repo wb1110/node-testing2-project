c19fcf508911367b7f4fcf1f11b25320d957b63c
wb1110 <strengthcoachwb@gmail.com>
2022-01-27 14:56:48 -0600

    migration

--- data/migrations/20220127190827_create-table.js
+++ data/migrations/20220127190827_create-table.js
@@ -1,3 +1,11 @@
-exports.up = function (knex) {};
+exports.up = function (knex) {
+  return knex.schema.createTable('testing', (table) => {
+    table.increments('testing_id');
+    table.string('full_name').notNullable().unique();
+    table.string('email').notNullable().unique();
+  });
+};
 
-exports.down = function (knex) {};
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists('testing');
+};
